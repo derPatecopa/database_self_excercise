ef0286dc0525674e83a89913684aa2f374f1e3bd
derPatecopa <mb89@me.com>
2023-02-09 11:30:22 +0100

    ready to go to migrations

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,32 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+// import express, { Request, Response } from "express";
+// import bodyParser from "body-parser";
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+// const app: express.Application = express();
+// const address: string = "0.0.0.0:3000";
 
-app.use(bodyParser.json())
+// app.use("/");
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+// app.get("/", function (req: Request, res: Response) {
+//   res.send("Database server running");
+// });
 
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+// app.listen(3000, function () {
+//   console.log(`starting app on: ${address}`);
+// });
+
+//implemented my own logic for server with nodemon and a little routing to /database
+
+import express, {Application} from "express";
+import mainroutes from "./routes/mainroutes";
+const app: Application = express();
+const port = 3000;
+
+//using app with routes
+app.use("/database", mainroutes);
+
+// start the Express server
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
+
+export default app;
