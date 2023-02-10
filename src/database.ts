a7ca854077861e7be8fe120eafbc940e35b6ea43
derPatecopa <mb89@me.com>
2023-02-10 11:51:07 +0100

    added .env variables to database.ts

--- src/database.ts
+++ src/database.ts
@@ -3,14 +3,26 @@ import { Pool } from "pg";
 
 dotenv.config();
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
+const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_TEST_DB, POSTGRES_USER, POSTGRES_PASSWORD, ENV } =
   process.env;
 
-const client = new Pool({
+let client : Pool | null = null;
+console.log(ENV);
+
+if (ENV === 'env') {
+client = new Pool({
   host: POSTGRES_HOST,
   database: POSTGRES_DB,
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
 });
-
+}
+if(ENV === 'test') {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD
+  })
+}
 export default client;
