8f1dd0284dc2b706b5f12c337031319c8acd502e
derPatecopa <mb89@me.com>
2023-02-10 11:40:15 +0100

    starting to add testing models

--- src/models/book.ts
+++ src/models/book.ts
@@ -24,12 +24,14 @@ export class BookStore {
     }
   }
 
+  //return value not an array since we want a single entry to display
   async show(id: string): Promise<Book> {
     try {
+      //$1 is a placeholder for the value provided from show
       const sql = "SELECT * FROM books WHERE id=($1)";
 
       const conn = await Client.connect();
-
+      //the value is passed from id to the sql query in square brackets in the query method
       const result = await conn.query(sql, [id]);
 
       conn.release();
@@ -39,21 +41,22 @@ export class BookStore {
       throw new Error(`Could not find book ${id}. Error: ${err}`);
     }
   }
-
+  //can use the book type for create instead of typing out all the single values
   async create(b: Book): Promise<Book> {
     try {
+      //same procedure here: $1... is placeholder for the incoming values
       const sql =
         "INSERT INTO books (title, author, total_pages, summary) VALUES($1, $2, $3, $4) RETURNING *";
 
       const conn = await Client.connect();
-
+      //and they are being passed here as an array in the query method
       const result = await conn.query(sql, [
         b.title,
         b.author,
         b.totalPages,
         b.summary,
       ]);
-
+//return a specific row
       const book = result.rows[0];
 
       conn.release();
