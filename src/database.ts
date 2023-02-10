2193efb4bbd9c16331a8ffe65cbbe7930aa6e1eb
derPatecopa <mb89@me.com>
2023-02-10 12:23:11 +0100

    added env variable to .env

--- src/database.ts
+++ src/database.ts
@@ -3,26 +3,32 @@ import { Pool } from "pg";
 
 dotenv.config();
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_TEST_DB, POSTGRES_USER, POSTGRES_PASSWORD, ENV } =
-  process.env;
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_TEST_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  ENV,
+} = process.env;
 
-let client : Pool | null = null;
+let client: Pool | null = null;
 console.log(ENV);
 
-if (ENV === 'env') {
-client = new Pool({
-  host: POSTGRES_HOST,
-  database: POSTGRES_DB,
-  user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-});
+if (ENV === "env") {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
 }
-if(ENV === 'test') {
+if (ENV === "test") {
   client = new Pool({
     host: POSTGRES_HOST,
     database: POSTGRES_TEST_DB,
     user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD
-  })
+    password: POSTGRES_PASSWORD,
+  });
 }
 export default client;
--- src/models/book.ts
+++ src/models/book.ts
@@ -2,10 +2,10 @@ import Client from "../database";
 
 const defaultBook: Book = {
   id: 0,
-  title: '',
-  author: '',
+  title: "",
+  author: "",
   totalPages: 0,
-  summary: ''
+  summary: "",
 };
 
 export type Book = {
@@ -18,99 +18,98 @@ export type Book = {
 
 export class BookStore {
   async index(): Promise<Book[]> {
-    if (Client){
-    try {
-      const conn = await Client.connect();
-      const sql = "SELECT * FROM books";
+    if (Client) {
+      try {
+        const conn = await Client.connect();
+        const sql = "SELECT * FROM books";
 
-      const result = await conn.query(sql);
+        const result = await conn.query(sql);
 
-      conn.release();
+        conn.release();
 
-      return result.rows;
-    } catch (err) {
-      throw new Error(`Could not get books. Error: ${err}`);
-    }
-    }
-    else{
-    console.log("Client is falsy")
-    return [];
+        return result.rows;
+      } catch (err) {
+        throw new Error(`Could not get books. Error: ${err}`);
+      }
+    } else {
+      console.log("Client is falsy");
+      return [];
     }
   }
 
   //return value not an array since we want a single entry to display
   async show(id: string): Promise<Book> {
-    if(Client) {
-    try {
-      //$1 is a placeholder for the value provided from show
-      const sql = "SELECT * FROM books WHERE id=($1)";
+    if (Client) {
+      try {
+        //$1 is a placeholder for the value provided from show
+        const sql = "SELECT * FROM books WHERE id=($1)";
 
-      const conn = await Client.connect();
-      //the value is passed from id to the sql query in square brackets in the query method
-      const result = await conn.query(sql, [id]);
+        const conn = await Client.connect();
+        //the value is passed from id to the sql query in square brackets in the query method
+        const result = await conn.query(sql, [id]);
 
-      conn.release();
+        conn.release();
 
-      return result.rows[0];
-    } catch (err) {
-      throw new Error(`Could not find book ${id}. Error: ${err}`);
+        return result.rows[0];
+      } catch (err) {
+        throw new Error(`Could not find book ${id}. Error: ${err}`);
+      }
+    } else {
+      console.log("Client is falsy");
+      return defaultBook;
     }
-  } else {
-    console.log("Client is falsy");
-    return defaultBook;
-  }
   }
   //can use the book type for create instead of typing out all the single values
   async create(b: Book): Promise<Book> {
     if (Client) {
-    try {
-      //same procedure here: $1... is placeholder for the incoming values
-      const sql =
-        "INSERT INTO books (title, author, total_pages, summary) VALUES($1, $2, $3, $4) RETURNING *";
+      try {
+        //same procedure here: $1... is placeholder for the incoming values
+        const sql =
+          "INSERT INTO books (title, author, total_pages, summary) VALUES($1, $2, $3, $4) RETURNING *";
 
-      const conn = await Client.connect();
-      //and they are being passed here as an array in the query method
-      const result = await conn.query(sql, [
-        b.title,
-        b.author,
-        b.totalPages,
-        b.summary,
-      ]);
-//return a specific row
-      const book = result.rows[0];
+        const conn = await Client.connect();
+        //and they are being passed here as an array in the query method
+        const result = await conn.query(sql, [
+          b.title,
+          b.author,
+          b.totalPages,
+          b.summary,
+        ]);
+        //return a specific row
+        const book = result.rows[0];
 
-      conn.release();
+        conn.release();
 
-      return book;
-    } catch (err) {
-      throw new Error(`Could not add new book ${b.title}. Error: ${err}`);
+        return book;
+      } catch (err) {
+        throw new Error(`Could not add new book ${b.title}. Error: ${err}`);
+      }
+    } else {
+      console.log("Client is falsy");
+      return defaultBook;
     }
-  } else {
-    console.log("Client is falsy");
-    return defaultBook;
-  }
   }
 
   async delete(id: string): Promise<Book> {
     if (Client) {
-    try {
-      const sql = "DELETE FROM books WHERE id=($1)";
+      try {
+        const sql = "DELETE FROM books WHERE id=($1)";
 
-      const conn = await Client.connect();
+        const conn = await Client.connect();
 
-      const result = await conn.query(sql, [id]);
+        const result = await conn.query(sql, [id]);
 
-      const book = result.rows[0];
+        const book = result.rows[0];
 
-      conn.release();
+        conn.release();
 
-      return book;
-    } catch (err) {
-      throw new Error(`Could not delete book ${id}. Error: ${err}`);
+        return book;
+      } catch (err) {
+        throw new Error(`Could not delete book ${id}. Error: ${err}`);
+      }
+    } else {
+      console.log("Client is falsy");
+      return defaultBook;
     }
-  } else {
-    console.log("Client is falsy");
-    return defaultBook;
-  }
   }
 }
