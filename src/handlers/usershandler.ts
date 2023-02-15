999c95d8987623b4e82eb412c557669e8cbceea2
derPatecopa <mb89@me.com>
2023-02-15 10:40:28 +0100

    added authentication and sign to usershandler

--- src/handlers/usershandler.ts
+++ src/handlers/usershandler.ts
@@ -1,51 +1,74 @@
-import express from 'express';
-import {User, UserStore} from '../models/users';
+import express from "express";
+import { User, UserStore } from "../models/users";
 import jwt, { Secret } from "jsonwebtoken";
 
 const store = new UserStore();
 
-export const index = async(
-    _req: express.Request, 
-    res: express.Response,
-    next: express.NextFunction
+export const index = async (
+  _req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
 ) => {
-    try {
-        const users = await store.index();
-        res.json(users);
-    } catch (err) {
-        res.send("Could not get users");
-        console.error(err);
-    }
-    next();
+  try {
+    const users = await store.index();
+    res.json(users);
+  } catch (err) {
+    res.send("Could not get users");
+    console.error(err);
+  }
+  next();
 };
 
 export const show = async (
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
 ) => {
-    try {
-        const user = await store.show(req.params.id);
-        res.json(user);
-    } catch (err) {
-        res.send("Could not get user");
-        console.error(err);
-    }
-    next();
+  try {
+    const user = await store.show(req.params.id);
+    res.json(user);
+  } catch (err) {
+    res.send("Could not get user");
+    console.error(err);
+  }
+  next();
 };
 
 export const create = async (req: express.Request, res: express.Response) => {
-    const user: User = {
-        user_name: req.body.username,
-        user_password: req.body.user_password
-    }
-    try {
-        const newUser = await store.create(user);
-        const token = jwt.sign({user: newUser}, (process.env.TOKEN_SECRET as unknown) as Secret);
-        res.json(token);
-    } catch(err) {
-        res.status(400);
-        res.json(user)
-    }
+  const user: User = {
+    user_name: req.body.username,
+    user_password: req.body.user_password,
+  };
+  try {
+    const newUser = await store.create(user);
+    const token = jwt.sign(
+      { user: newUser },
+      process.env.TOKEN_SECRET as unknown as Secret
+    );
+    res.json(token);
+  } catch (err) {
+    res.status(400);
+    res.json(user);
+  }
+};
 
-}
+export const authenticate = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  const user: User = {
+    user_name: req.body.username,
+    user_password: req.body.user_password,
+  };
+  try {
+    const u = await store.authenticate(user.user_name, user.user_password);
+    const token = jwt.sign(
+      { user: u },
+      process.env.TOKEN_SECRET as unknown as Secret
+    );
+    res.json(token);
+  } catch (error) {
+    res.status(401);
+    res.json({ error });
+  }
+};
